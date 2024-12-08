4980d892a9565696088e37407841b45e41cef2f6
Rayhan Muhammad <43977766+rayhanmhd123@users.noreply.github.com>
2024-12-08 21:23:27 +0700

    add explicit return type to useTemperatureData function

--- frontend/lib/useTemperatureData.ts
+++ frontend/lib/useTemperatureData.ts
@@ -5,8 +5,8 @@ import io from "socket.io-client";
 const socket = io("http://localhost:3001");
 
 export function useTemperatureData(
-  initialData: RoomTemperature[],
-) {
+  initialData: RoomTemperature[]
+): RoomTemperature[] {
   const [data, setData] = useState<RoomTemperature[]>(initialData);
 
   useEffect(() => {
